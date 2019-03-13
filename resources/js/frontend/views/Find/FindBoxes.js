e2efdc3e919e79fec5d89b338650533af28559f4
eredani <me@eredani.com>
2019-03-13 22:47:51 +0000

    Add maps

--- resources/js/frontend/views/Find/FindBoxes.js
+++ resources/js/frontend/views/Find/FindBoxes.js
@@ -1,14 +1,39 @@
-import React, {Component} from 'react';
-
-class FindBoxes extends Component {
-
+import React, { Component } from 'react';
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+const MyMapComponent = compose(
+    withProps({
+      /**
+       * Note: create and replace your own key in the Google console.
+       * https://console.developers.google.com/apis/dashboard
+       * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
+       */
+      googleMapURL:
+        "https://maps.googleapis.com/maps/api/js?key=AIzaSyB4XVXifE5FJzwpQ5Ffnem8ISm6tPvom4o&v=3.exp&libraries=geometry,drawing,places",
+      loadingElement: <div style={{ height: `100%` }} />,
+      containerElement: <div style={{ height: `400px` }} />,
+      mapElement: <div style={{ height: `100%` }} />
+    }),
+    withScriptjs,
+    withGoogleMap
+  )(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  ));
+export default class FindBoxes extends Component {
+    
+  
     render() {
         return (
-            <div className="animated fadeIn">
-                FindBoxes
-            </div>
+            <MyMapComponent isMarkerShown />
         )
     }
 }
-
-export default FindBoxes;
