323fe9e7906aeb41ef168e4eb94dc58051b5db09
eredani <me@eredani.com>
2019-03-20 04:48:30 +0000

    Fix

--- resources/js/frontend/index.js
+++ resources/js/frontend/index.js
@@ -6,7 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import Amplify from 'aws-amplify' // new
-import awsmobile from '../../../src/aws-exports';
+import awsmobile from '../../../awsmobilejs/#current-backend-info/aws-exports';
+
 Amplify.configure(awsmobile) // new
 ReactDOM.render(
     <App/>, document.getElementById('root'));
